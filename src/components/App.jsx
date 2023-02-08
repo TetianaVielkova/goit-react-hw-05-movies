ab21d28a4cc5f9a3b882b293b58472fdb353ea2e
TanyaVielkova <106912349+TetianaVielkova@users.noreply.github.com>
2023-02-08 23:17:43 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -46,7 +46,7 @@ export const App = () => {
           pauseOnFocusLoss
           draggable
           pauseOnHover
-          theme="colored"
+          theme="light"
           />
       </Navigation>
     </BrowserRouter>
